319689b799ed772ddb680d80e18c90cb0e98dc5d
bruno <brunoalbuquerquebrito@outlook.com>
2018-10-07 13:02:23 -0300

    Criando servidor e respondendo a requisicao

--- noticias.js
+++ noticias.js
@@ -1,1 +1,9 @@
-console.log('Criando um site de noticias com NodeJs')
+//require incorpora outros arquivos arquivos
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+
+    res.end("<html><body>Portal de noticias</body></html>")
+});
+console.log("Servidor funcionando");
+server.listen(3000);
